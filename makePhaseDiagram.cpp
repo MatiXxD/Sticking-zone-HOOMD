73ad87166e02eb53e5c29194a5dacfbbe7d4d0ff
MatiXxD <dxxmatixxd@gmail.com>
2022-08-30 15:30:56 +0300

    Little changes in PhaseDiagram code

--- makePhaseDiagram.cpp
+++ makePhaseDiagram.cpp
@@ -2,8 +2,10 @@
 #include <fstream>
 #include <string>
 
-#define EPS_NUM 14
+#define EPS_NUM 13
 
+//0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0
+//1    2    3    3    4    5    6    7    8    9    10   11   12   13   14   15   16   17   18
 
 class MakeDiagram{
 
@@ -13,12 +15,9 @@ class MakeDiagram{
     const unsigned N = 3600;
     const unsigned SPLIT_PARAM = 15;
     const unsigned FRAMES_HANDLE = 10;
-    const unsigned FRAMES_SKIP = 200;
-    const unsigned epsArray[EPS_NUM] = {1, 3, 5, 7, 9, 11, 13, 15,
-        17, 19, 21, 23, 25, 27};
+    const unsigned FRAMES_SKIP = 20;
 
-    const double LEN_X = 569.8768003947 * 2.0;
-    
+    const double LEN_X = 341.9260802368 * 2.0;
 
     double *x;
     double *arrN;
@@ -40,15 +39,20 @@ MakeDiagram::MakeDiagram(std::string dir, std::string name){
     x = new double[N];
     arrN = new double[SPLIT_PARAM];
 
+    int skip;
     for(int rep = 0 ; rep < EPS_NUM ; rep++){
         
         // Open file to read data 
-        std::string nameOut = dir + name + std::to_string(epsArray[rep]) + ".txt";
-        std::cout << "Work with file : " << nameOut << '\n';
+        std::string nameOut = dir + name + std::to_string(rep+1) + ".txt";
+        std::cout << "Repeat: " << rep << std::endl;
         f.open(nameOut.c_str());
 
         // Skip frames which correspond to system's relaxation
-        for(int i = 0 ; i < FRAMES_SKIP ; i++){
+        if(rep != 0)
+            skip = FRAMES_SKIP;
+        else 
+            skip = FRAMES_SKIP * 10;
+        for(int i = 0 ; i < skip ; i++){
             readHeader();
             skipFrame();
         }
@@ -127,8 +131,10 @@ void MakeDiagram::getN(){
 
     for(int i = 0 ; i < N ; i++){
         temp = (x[i] + LEN_X / 2.0) / (LEN_X / SPLIT_PARAM);
-        if(temp < SPLIT_PARAM) arrN[temp]++;
-        else arrN[SPLIT_PARAM-1]++;
+        if(temp < SPLIT_PARAM) 
+            arrN[temp]++;
+        else 
+            arrN[SPLIT_PARAM-1]++;
     }
 
 }
@@ -136,11 +142,11 @@ void MakeDiagram::getN(){
 void MakeDiagram::writeRes(int outNum){
  
     std::ofstream fout;
-    std::string name = "/home/ilya/MD/phaseDiagram/dump/main/dump_" + std::to_string(epsArray[outNum]) + ".txt";
+    std::string name = "/home/ilya/MD/phaseDiagram/dump/main2/dump_" + std::to_string(outNum+1) + ".txt";
     fout.open(name.c_str());
 
     for(int i = 0 ; i < SPLIT_PARAM ; i++)
-        fout << i * outNum * (LEN_X / SPLIT_PARAM) << ' ' << arrN[i] / (N * SPLIT_PARAM) << '\n';
+        fout << i * (LEN_X / SPLIT_PARAM) << ' ' << arrN[i] / (N * FRAMES_HANDLE) << '\n';
 
     fout.close();
 
@@ -148,7 +154,7 @@ void MakeDiagram::writeRes(int outNum){
 
 int main(int argc, char* argv[]){
 
-    std::string dir = "/home/ilya/MD/phaseDiagram/dump/hoomdDump/";
+    std::string dir = "/home/ilya/MD/phaseDiagram/dump/hoomdDump2/";
     std::string name = "d_v";
 
     MakeDiagram(dir, name);
